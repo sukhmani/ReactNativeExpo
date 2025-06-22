70a79a759eb613c12e522bfef52f14f183221a61
Sukhmani <thukralsukhmani@cityuniversity.edu>
2025-06-21 23:03:31 -0700

    updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
+const path = require('path');
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function(env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
+
+  config.resolve.alias = {
+    ...(config.resolve.alias || {}),
+    app: path.resolve(__dirname, 'app')
+  };
 
   return config;
 };
